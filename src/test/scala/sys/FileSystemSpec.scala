a0103f1eea3b24c42684e0b7df20d0cd996c537b
Mitch Comardo <comamitc@comamitc-mac.local>
2014-10-30 22:43:26 -0500

    adding test case for base directory

--- src/test/scala/sys/FileSystemSpec.scala
+++ src/test/scala/sys/FileSystemSpec.scala
@@ -9,11 +9,18 @@ class FileSystemSpec extends FunSuite with Matchers {
     fs.listFiles.size should be(3)
   }
 
-  test("FileSystem#listFiles(./test_files/*.log) should find 1 file") {
+  test("FileSystem#listFiles(./test_files/*.log) should find 2 files") {
     val fs = new FileSystem("./test_files/*.log")
     val files = fs.listFiles
     println(files)
     files.size should be(2)
+  }
+
+  test("FileSystem#listFiles(test_files/*.log) should find 2 file") {
+    val fs = new FileSystem("./*.log")
+    val files = fs.listFiles
+    println(files)
+    files.size should be(4)
   }
 
   test("FileSystem#listFiles should throw exception with invalid path") {
